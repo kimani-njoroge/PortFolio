02669c436b191af186f7845cd07b8edccce29fc2
kimani-njoroge <kimani064@gmail.com>
2018-08-26 22:45:14 +0300

    add home view and urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.index,name = 'index')
+    url('^$',views.home,name = 'home'),
+    url('^index/',views.index,name = 'index')
 ]
