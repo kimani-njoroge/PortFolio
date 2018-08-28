2087e2baee4164bf48da2d856adb4824ce6e5b98
kimani-njoroge <kimani064@gmail.com>
2018-08-28 23:53:36 +0300

    add skills section add about me style about me and skills section

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
-    url('^index/',views.index,name = 'index')
+    url('^$',views.index,name = 'index')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from .models import Project
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return render(request,'home.html')
-
-
 def index(request):
 
     projects = Project.get_projects()
